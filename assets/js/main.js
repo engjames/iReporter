5925f97047d6fca09f150e1e0d288ded06ceda8b
james kisuule <getjames20@gmail.com>
2018-11-21 16:23:26 +0300

    modify javascript and add auth

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ function authenticateUsers(){
   var email = document.querySelector("#email").value;
   var password = document.querySelector("#field").value;
   if(email==='user@gmail.com' && password ==="user123"){
-     window.location.href ="home.html";
+     window.location.href ="red_flag.html";
   }else if(email==='admin@gmail.com' && password ==="admin123"){
     var email = document.querySelector("#email").value;
     var password = document.querySelector("#field").value;
