960d3dbc9487f98b667d1b72219e0ccffc630615
james kisuule <kisuulejames1@gmail.com>
2018-11-26 13:01:18 +0300

    Add get all red-flag records end point[#162190218]

--- app/view/redflag_view.py
+++ app/view/redflag_view.py
@@ -28,9 +28,6 @@ class RedFlagView(MethodView):
             return jsonify({"status":404, "data":[{"error-message" : "No red-flag found"}]})
         return jsonify({"status": 400, "data":[{"error-message" : "id should be a non negative integer"}]})
 
-
-
-
     # Create a red-flag record
     def post(self):
         if request.content_type == 'application/json':
@@ -72,46 +69,9 @@ class RedFlagView(MethodView):
             return jsonify({"status":400, "data": [{'error-message' : 'wrong body format. follow this example ->> '+'{“createdBy”:​James​, “location”:​​[12.4567,3.6789]​, “comment”:​collapsed bridges}'}]})
         return jsonify({"status":202, "data":[{'error-message' : 'Content-type must be json'}]}) 
 
-    def put(self, id):
-        try:
-            redflag_id = int(id)
-        except:
-            return jsonify({"status": 400, "data":[{"error-message":"id should be a non negative integer" }]})
-        
-        if redflag_id > 0:
-            if request.content_type == 'application/json':
-                if 'status' in request.json and isinstance(request.json['status'], str):
-                    redflag_json = request.get_json()
-                    for redflag_record in self.redflag_list:
-                        if redflag_record.__dict__['id'] == redflag_id:
-                            redflag_record.__dict__['status'] = redflag_json['status']
-                            return jsonify({"status":400, "data":[{"id":redflag_id, "message":"Updated red-flag record’s location"}]})
-                    return jsonify({"status":404, "data": [{"error-message" : "No red-flag found"}]})
-                return jsonify({"status": 400, "data":[{"error-message" : "wrong body format. follow this example ->> {'status':'under investigation'}"}]})
-            return jsonify({"status":202, "data":[{'error-message' : 'Content-type must be json'}]})
-        return jsonify({"status": 400, "data":[{"error-message" : "id cannot be a negative"}]})
-        
-
-
-    # Delete a specific red flag record
-    def delete(self, id):
-        try:
-            redflag_id = int(id)
-        except:
-           return jsonify({"status": 400, "data":[{"error-message" : "id should be a non negative integer"}]})
-
-        if redflag_id > 0:
-            for redflag_record in self.redflag_list:
-                if redflag_record.__dict__['id'] == redflag_id:
-                    self.redflag_list.remove(redflag_record)
-                    return jsonify({"status":200, "data": self.redflag_list})
-            return jsonify({"status":404, "data": [{"error-message" : "No red-flag found"}]})
-        return jsonify({"status": 400, "data":[{"error-message" : "id cannot be a negative"}]})
-
-
-
 class RedFlagUrls:
     @staticmethod
     def fetch_urls(app):
         redflag_view  = RedFlagView.as_view('ireporter')
         app.add_url_rule('/api/v1/red-flags', view_func=redflag_view, methods=['POST'])
+        app.add_url_rule('/api/v1/red-flags', defaults={'id': None}, view_func=redflag_view, methods=['GET',])
