437fb82b19e601190da70bb68c3f777f89790b7c
james kisuule <kisuulejames1@gmail.com>
2018-11-28 15:18:13 +0300

    modify test file

--- tests/test_redflags_views.py
+++ tests/test_redflags_views.py
@@ -5,7 +5,7 @@ CLIENT = app.test_client
 
 ############################# Tests when there are no red-flags ######################################
 def test_when_there_are_no_redflags():
-    result = CLIENT().get('/red-flags')
+    result = CLIENT().get('/api/v1/red-flags')
     assert result.status_code == 200
     json_data = json.loads(result.data)
     assert json_data['data'][0]['message'] == "No red-flags found. Please create one."
@@ -16,7 +16,7 @@ def test_to_create_a_new_redflag():
     """
     Method for addng a new red-flag
     """
-    result = CLIENT().post('/red-flags', content_type='application/json',
+    result = CLIENT().post('/api/v1/red-flags', content_type='application/json',
                            data=json.dumps({"createdBy" : "James",
                                             "location" : [8.6784, 2.5673],
                                             "comment" : "collapsed bridges"}))
@@ -28,7 +28,7 @@ def test_to_create_a_new_redflag():
     assert json_data['data'][0]['message'] == "Created red-flag record"
 
     #make a get request to check whether the red-flag exists
-    check_redflag = CLIENT().get('/red-flags')
+    check_redflag = CLIENT().get('/api/v1/red-flags')
     assert check_redflag.status_code == 200
     json_data = json.loads(check_redflag.data)
     assert json_data['data'][0]['id'] == 1
@@ -43,7 +43,7 @@ def test_to_get_all_redflags():
     """
     Method for fetching all red-flags.
     """
-    result = CLIENT().get('/red-flags')
+    result = CLIENT().get('/api/v1/red-flags')
     assert result.status_code == 200
     json_data = json.loads(result.data)
     assert json_data['data'][0]['id'] == 1
@@ -58,7 +58,7 @@ def test_to_get_a_specific_redflags():
     """
     Method for fetching a specific red-flag
     """
-    result = CLIENT().get('/red-flags/1')
+    result = CLIENT().get('/api/v1/red-flags/1')
     assert result.status_code == 200
     json_data = json.loads(result.data)
     assert json_data['data']['id'] == 1
@@ -73,7 +73,7 @@ def test_to_change_geolocation_of_a_redflag():
     """
     Method for changing geolocation
     """
-    result = CLIENT().put('/red-flags/1', content_type='application/json',
+    result = CLIENT().put('/api/v1/red-flags/1', content_type='application/json',
                            data=json.dumps({"location" : [0.9090,5.9090]}))
     
     assert result.status_code == 200
@@ -84,7 +84,7 @@ def test_to_change_geolocation_of_a_redflag():
     assert json_data['data'][0]['message'] == "Updated red-flag record’s location"
 
     #make a put request to check whether the red-flag has been updated
-    check_redflag = CLIENT().get('/red-flags/1')
+    check_redflag = CLIENT().get('/api/v1/red-flags/1')
     assert check_redflag.status_code == 200
     json_data = json.loads(check_redflag.data)
     assert json_data['data']['id'] == 1
@@ -96,7 +96,7 @@ def test_to_change_geolocation_of_a_redflag():
 
 ############################# Tests for deleting a red flag ######################################
 def test_to_delete_a_redflag():
-    result = CLIENT().delete('/red-flags/1', content_type='application/json',
+    result = CLIENT().delete('/api/v1/red-flags/1', content_type='application/json',
                            data=json.dumps({"createdBy":"James"}))
     
     assert result.status_code == 200
@@ -107,7 +107,7 @@ def test_to_delete_a_redflag():
     assert json_data['data'][0]['message'] == "red-flag record has been deleted"
 
     # check to verify whether the red-flag has been deleted
-    result = CLIENT().get('/red-flags/1')
+    result = CLIENT().get('/api/v1/red-flags/1')
     assert result.status_code == 200
     json_data = json.loads(result.data)
     assert json_data['data'][0]['error-message'] == "No red-flag found"
@@ -117,7 +117,7 @@ def test_for_updating_redflag_status():
     # make sure there is a red-flag to update
     test_to_create_a_new_redflag()
 
-    result = CLIENT().put('/update-red-flags/1', content_type='application/json',
+    result = CLIENT().put('/api/v1/update-red-flags/1', content_type='application/json',
                            data=json.dumps({"status" : "under investigation"}))
 
     assert result.status_code == 200
@@ -129,7 +129,7 @@ def test_for_updating_redflag_status():
 
 ###########Tests for checking whether the user can delete a red-flag when the status is promoted###############  
 def test_to_delete_a_redlag_after_status_update():
-    result = CLIENT().delete('/red-flags/1', content_type='application/json',
+    result = CLIENT().delete('/api/v1/red-flags/1', content_type='application/json',
                                         data=json.dumps({"createdBy":"James"}))
     assert result.status_code == 200
     json_data = json.loads(result.data)
@@ -137,7 +137,7 @@ def test_to_delete_a_redlag_after_status_update():
 
 ###########Tests for checking whether the user can change geolocation after red-flag status update###############  
 def test_to_change_geolocation_after_status_update():
-    result = CLIENT().put('/red-flags/1', content_type='application/json',
+    result = CLIENT().put('/api/v1/red-flags/1', content_type='application/json',
                            data=json.dumps({"location" : [0.9090,5.9090]}))
     assert result.status_code == 200
     json_data = json.loads(result.data)
@@ -146,18 +146,18 @@ def test_to_change_geolocation_after_status_update():
 ############################# Tests for updating the status of a red-flag ######################################
 def test_for_updating_redflag_status_with_wrong_values():
     
-    result1 = CLIENT().put('/update-red-flags/1', content_type='application/json',
+    result1 = CLIENT().put('/api/v1/update-red-flags/1', content_type='application/json',
                            data=json.dumps({"status" : "fhfhfhfhfhf"}))
-    result2 = CLIENT().put('/update-red-flags/1', content_type='application/json',
+    result2 = CLIENT().put('/api/v1/update-red-flags/1', content_type='application/json',
                            data=json.dumps({}))
 
-    result3 = CLIENT().put('/update-red-flags/-1', content_type='application/json',
+    result3 = CLIENT().put('/api/v1/update-red-flags/-1', content_type='application/json',
                            data=json.dumps({}))
     
-    result4 = CLIENT().put('/update-red-flags/james', content_type='application/json',
+    result4 = CLIENT().put('/api/v1/update-red-flags/james', content_type='application/json',
                            data=json.dumps({}))
     
-    result5 = CLIENT().put('/update-red-flags/1', content_type='text',
+    result5 = CLIENT().put('/api/v1/update-red-flags/1', content_type='text',
                            data=json.dumps({}))
 
     assert result1.status_code == 200
@@ -189,22 +189,22 @@ def test_to_change_geolocation_of_a_redflag_with_wrong_values():
     """
     Method for changing geolocation
     """
-    result1 = CLIENT().put('/red-flags/1', content_type='application/json',
+    result1 = CLIENT().put('/api/v1/red-flags/1', content_type='application/json',
                            data=json.dumps({"location" : 4}))
     
-    result2 = CLIENT().put('/red-flags/1', content_type='application/json',
+    result2 = CLIENT().put('/api/v1/red-flags/1', content_type='application/json',
                            data=json.dumps({"location" : "fhfhf"}))
-    result3 = CLIENT().put('/red-flags/1', content_type='application/json',
+    result3 = CLIENT().put('/api/v1/red-flags/1', content_type='application/json',
                            data=json.dumps({"location" : True}))
-    result4 = CLIENT().put('/red-flags/james', content_type='application/json',
+    result4 = CLIENT().put('/api/v1/red-flags/james', content_type='application/json',
                            data=json.dumps({"location" : [2,2]}))
     
-    result5 = CLIENT().put('/red-flags/1', content_type='application/json',
+    result5 = CLIENT().put('/api/v1/red-flags/1', content_type='application/json',
                            data=json.dumps({"location" : ["2","2"]}))
 
-    result6 = CLIENT().put('/red-flags/1', content_type='application/json',
+    result6 = CLIENT().put('/api/v1/red-flags/1', content_type='application/json',
                            data=json.dumps({"location" : [2,2,2]}))
-    result7 = CLIENT().put('/red-flags/-1', content_type='application/json',
+    result7 = CLIENT().put('/api/v1/red-flags/-1', content_type='application/json',
                            data=json.dumps({"location" : [2,2]}))
 
     results = [result1, result2, result3]
@@ -232,9 +232,9 @@ def test_to_change_geolocation_of_a_redflag_with_wrong_values():
 
 #########################tests for updating a red-flag that doesn't exist####################################
 def test_to_update_a_redflag_which_doesnt_exist():
-    result1 = CLIENT().put('/red-flags/10000', content_type='application/json',
+    result1 = CLIENT().put('/api/v1/red-flags/10000', content_type='application/json',
                                         data=json.dumps({"location" : [2,3]}))
-    result2 = CLIENT().put('/update-red-flags/100000', content_type='application/json',
+    result2 = CLIENT().put('/api/v1/update-red-flags/100000', content_type='application/json',
                            data=json.dumps({"status" : "under investigation"}))
 
     results = [result1, result2]
@@ -247,7 +247,7 @@ def test_to_update_a_redflag_which_doesnt_exist():
 
 #########################tests for deleting a red-flag that doesn't exist####################################
 def test_to_delete_a_redflag_which_doesnt_exist():
-    result = CLIENT().delete('/red-flags/10000', content_type='application/json',
+    result = CLIENT().delete('/api/v1/red-flags/10000', content_type='application/json',
                                         data=json.dumps({"createdBy" : "James"}))
 
     assert result.status_code == 200
@@ -258,9 +258,9 @@ def test_to_delete_a_redflag_which_doesnt_exist():
 
 #########################tests for getting a red-flag that doesn't exist####################################
 def test_to_get_a_redflag_which_doesnt_exist():
-    result1 = CLIENT().get('/red-flags/10000')
-    result2 = CLIENT().get('/red-flags/-3')
-    result3 = CLIENT().get('/red-flags/james')
+    result1 = CLIENT().get('/api/v1/red-flags/10000')
+    result2 = CLIENT().get('/api/v1/red-flags/-3')
+    result3 = CLIENT().get('/api/v1/red-flags/james')
 
     assert result1.status_code == 200
     assert result2.status_code == 200
@@ -284,15 +284,15 @@ def test_to_get_a_redflag_which_doesnt_exist():
 
 #########################tests for making a request with data which is not in json format #################
 def test_to_make_a_request_with_wrong_content_type():
-    result1 = CLIENT().post('/red-flags', content_type='text',
+    result1 = CLIENT().post('/api/v1/red-flags', content_type='text',
                            data=json.dumps({"createdBy" : "James",
                                             "location" : [8.6784, 2.5673],
                                             "comment" : "collapsed bridges"}))
 
-    result2 = CLIENT().put('/red-flags/1', content_type='text',
+    result2 = CLIENT().put('/api/v1/red-flags/1', content_type='text',
                                         data=json.dumps({"location" : [2,3]}))
 
-    result3 = CLIENT().delete('/red-flags/1', content_type='text',
+    result3 = CLIENT().delete('/api/v1/red-flags/1', content_type='text',
                                         data=json.dumps({"createdBy" : "James"}))
     results = [result1, result2, result3]
     for result in results:
@@ -304,19 +304,19 @@ def test_to_make_a_request_with_wrong_content_type():
 
 ############################ test to create a red-flag with wrong data ###############################
 def test_to_create_a_redflag_with_wrong_data():
-    result1= CLIENT().post('/red-flags', content_type='application/json',
+    result1= CLIENT().post('/api/v1/red-flags', content_type='application/json',
                            data=json.dumps({}))
      
-    result2 = CLIENT().post('/red-flags', content_type='application/json',
+    result2 = CLIENT().post('/api/v1/red-flags', content_type='application/json',
                            data=json.dumps({"createdBy" : "",
                                             "location" : [],
                                             "comment" : ""}))
-    result3 = CLIENT().post('/red-flags', content_type='application/json',
+    result3 = CLIENT().post('/api/v1/red-flags', content_type='application/json',
                            data=json.dumps({"createdBy" : 3,
                                             "location" : False,
                                             "comment" : True}))
 
-    result4 = CLIENT().post('/red-flags', content_type='application/json',
+    result4 = CLIENT().post('/api/v1/red-flags', content_type='application/json',
                            data=json.dumps({"createdBy" : "James",
                                             "location" : ["12.3453", "45.2123"],
                                             "comment" : "collapsed bridges"}))
@@ -350,9 +350,9 @@ def test_to_create_a_redflag_with_wrong_data():
 
 ######################## tests for deleting a red-flag with wrong username #######################################
 def test_delete_redflag_with_wrong_username():
-    result1 = CLIENT().delete('/red-flags/1', content_type='application/json',
+    result1 = CLIENT().delete('/api/v1/red-flags/1', content_type='application/json',
                                         data=json.dumps({"createdBy":"dgfhdjdj"}))
-    result2 = CLIENT().delete('/red-flags/1', content_type='application/json',
+    result2 = CLIENT().delete('/api/v1/red-flags/1', content_type='application/json',
                                         data=json.dumps({}))
     assert result1.status_code == 200
     assert result2.status_code == 200
@@ -364,7 +364,7 @@ def test_delete_redflag_with_wrong_username():
 
 ############################# tests for checking whethera red-flag already exists #################################
 def test_whether_a_redflag_already_exists():
-    result = CLIENT().post('/red-flags', content_type='application/json',
+    result = CLIENT().post('/api/v1/red-flags', content_type='application/json',
                            data=json.dumps({"createdBy" : "James",
                                             "location" : [8.6784, 2.5673],
                                             "comment" : "collapsed bridges"}))
@@ -376,9 +376,9 @@ def test_whether_a_redflag_already_exists():
 
 # ######################### tests for deleting a red-flag using a wrong id ##################################
 def test_to_delete_a_redflag_using_a_wrong_id():
-    result1 = CLIENT().delete('/red-flags/james', content_type='application/json',
+    result1 = CLIENT().delete('/api/v1/red-flags/james', content_type='application/json',
                                         data=json.dumps({"createdBy" : "James"}))
-    result2 = CLIENT().delete('/red-flags/-1', content_type='application/json',
+    result2 = CLIENT().delete('/api/v1/red-flags/-1', content_type='application/json',
                                         data=json.dumps({"createdBy" : "James"}))
 
     assert result1.status_code == 200
